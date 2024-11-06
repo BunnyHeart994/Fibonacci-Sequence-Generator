f5b99cbe6544d19c0443ae7a014a60c5c815b309
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2024-11-06 20:26:29 -0300

    Work in progress

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main
     public static final Scanner scan = new Scanner(System.in);
     public static void main(String[] args)
     {
-        int prev, next, result, limit;
+        int prev = 1, next = 2, result, limit;
         System.out.print("THIS PROGRAM WILL GENERATE THE FIBONACCI " +
-                "SEQUENCE THAT WILL GO UP TO THE NUMBER YOU CHOSE (\"limit\").\n" +
+                "SEQUENCE THAT WILL GO UP TO THE NUMBER YOU CHOSE (\"limit\").\n\n" +
                 "Input the limit: ");
         while(true)
         {
@@ -21,6 +21,29 @@ public class Main
                 System.out.print("Only integers are accepted.\nTry again: ");
             }
         }
-
+        System.out.print("Sequence: 1, 2, ");
+        for (int i = 0; i <= limit; i++)
+        {
+            result = prev + next;
+            if (result <= limit)
+                System.out.print(String.join(", ", Integer.toString(result)));
+            prev = next;
+            next = result;
+        }
+    }
+    private static String chain(String... str)
+    {
+        int prev, next, limit, result, i;
+        for (i = 0; i <= limit; i++)
+        {
+            result = prev + next;
+            if (result <= limit)
+                System.out.print(String.join(", ", Integer.toString(result)));
+            prev = next;
+            next = result;
+        }
+        int[] resultArr = new int[i];
+        str =
+        return str;
     }
 }
