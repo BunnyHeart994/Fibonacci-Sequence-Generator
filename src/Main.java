3564b2847c051df4e56e933bfba726237a63635a
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2024-11-08 12:36:15 -0300

    Version 1 fixing work

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,19 @@ import java.util.Scanner;
 public class Main
 {
     public static final Scanner scan = new Scanner(System.in);
-    public static int prev = 1, next = 2, limit;
-    public static long result;
+    private static int limit;
+    private static long prev = 1, next = 2, result;
+
     public static void main(String[] args)
     {
         System.out.print("THIS PROGRAM WILL GENERATE THE FIBONACCI " +
                 "SEQUENCE THAT WILL GO UP TO THE NUMBER YOU CHOSE (\"limit\").\n\n" +
-                "Input the limit: ");
+                "Input the limit (up to ?): ");
         while(true)
         {
             try
             {
-                //while (true)
-                //{
-                    limit = scan.nextInt(); //NEEDS FIXING WORK
-                //}
+                limit = scan.nextInt();
                 break;
             }
             catch (Exception e)
@@ -39,15 +37,21 @@ public class Main
             case 2: System.out.print("Sequence: 1, 2.\nEND"); return;
             default: System.out.print("Sequence: 1, 2") ;break;
         }
-        int[] str = new int[limit - 2];
+        long[] str = new long[limit - 2];
 
         for(int i = 0; i < str.length; i++)
         {
-            result = prev + next;
+            if (prev + next < 0)
+            {
+                System.out.println("... (MAXIMUM LONG VALUE EXCEEDED, ENDING PROCESS...)\nEND");
+                break;
+            }
+            else
+                result = prev + next;
             str[i] = result;
             if (i <= str.length - 2)
                 System.out.print(", " + str[i]);
-            else System.out.print(", " + str[i] + ".");
+            else System.out.print(", " + str[i] + ".\nEND");
             prev = next;
             next = result;
         }
